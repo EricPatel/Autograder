2c76dc1aac74e22d37ce2cd4e6ffb302cc0fccb3
Eric Patel <ericpatel@nbp-253-173.nbp.ruw.rutgers.edu>
2019-04-18 17:33:02 -0400

    Added Python AutoGrader folder

---
+++ python/try.py
@@ -0,0 +1,21 @@
+import os
+
+tests = []
+results = []
+
+for file in os.listdir("tests"):
+    tests.append(file)
+
+for file in os.listdir("results"):
+    results.append(file)
+
+for x in range(len(tests)):
+    user = os.popen("cat tests/" + tests[x] + " | python3 rpn.py").read()
+    result = os.popen("cat results/" + results[x]).read()
+    if user == result :
+        print("Passed " + tests[x])
+    else:
+        print("Failed " + tests[x])
+
+
+#support python, java, C
