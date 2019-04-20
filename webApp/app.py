7714e2992d82085cb784b74f0d2be3e129949df5
Eric Patel <ericpatel@Erics-MBP-2.home>
2019-04-20 15:25:48 -0400

    Added routing for new pages and updated css

--- webApp/app.py
+++ webApp/app.py
@@ -1,14 +1,36 @@
 # Run with command [FLASK_APP=app.py FLASK_DEBUG=1 flask run]
 # Will reload on save
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
+#routing for the main page
 @app.route('/')
 def main():
     return render_template('pages/main.html')
 
+#routing for the login page
 @app.route('/login')
 def login():
     return render_template('pages/login.html')
+
+#check the login request
+@app.route('/login', methods=["POST"])
+def validateLogin():
+    #redirect as post
+    return redirect(url_for('dashboard'), code=307)
+
+#routing for the dashboard
+@app.route('/dashboard', methods=["POST"])
+def dashboard():
+    return render_template('pages/dashboard.html')
+
+@app.route('/assignment', methods=["POST"])
+def assignment():
+    return render_template('pages/assignment.html')
+
+@app.route('/signout', methods=["POST"])
+def signOut():
+    #redirect as post
+    return redirect('/')
