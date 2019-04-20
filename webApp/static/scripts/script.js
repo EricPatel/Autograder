f345fc7cf53dd23d2a33fbaa5acef9c4c67fac57
Eric Patel <ericpatel@Erics-MBP-2.home>
2019-04-19 23:06:55 -0400

    Finished Log In / Sign Up page

--- webApp/static/scripts/script.js
+++ webApp/static/scripts/script.js
@@ -1,9 +1,9 @@
 function show(evt, pane) {
   // Declare all variables
   var i, tabcontent, tablinks;
-
+  console.log(pane);
   // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = document.getElementsByClassName("panes");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
@@ -11,10 +11,10 @@ function show(evt, pane) {
   // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace(" r", "");
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(pane).style.display = "block";
-  evt.currentTarget.className += " active";
+  evt.currentTarget.className += " r";
 }
